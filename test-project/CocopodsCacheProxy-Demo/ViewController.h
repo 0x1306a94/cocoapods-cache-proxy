255d8c45d6ec334323bf54cdfb3b7f3d7cbc3ac0
0x1306a94 <0x1306a94@gmail.com>
2020-01-14 15:26:17 +0800

    添加测试工程

---
+++ test-project/CocopodsCacheProxy-Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocopodsCacheProxy-Demo
+//
+//  Created by king on 2020/1/7.
+//  Copyright © 2020 K&K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test-project/CocopodsCacheProxy-Demo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  CocopodsCacheProxy-Demo
+//
+//  Created by king on 2020/1/7.
+//  Copyright © 2020 K&K. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+
+@end
